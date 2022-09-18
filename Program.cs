6dd072976b385f3346f7b6c1a135c1e5796f6d8e
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-09-18 21:37:03 +0200

    prima funzione fatta

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+//preso un array di numeri interi, stampa a video il contenuto dell’array in questa forma “[elemento 1, elemento 2, elemento 3, ...].
+
+int[] numbers = { 1, 5, 7, 9, 11, 13, 17, 19, 23, 29 };
+
+void StampaArray(int[] array, char openP ='[', char closedP =']')
+{
+	Console.Write(openP);
+
+	int i;
+
+	for (i = 0; i < array.Length - 1; i++)
+	{
+		Console.Write(array[i]);
+        Console.Write(", ");
+    }
+
+    Console.Write(array[i]);
+    Console.Write(closedP);
+}
+
+StampaArray(numbers);
